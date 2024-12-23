335d4290f8289714b5779fad25e977e35b5f5a3b
hugotonette <hugo.tonette@gmail.com>
2024-12-23 13:05:06 -0300

    cleaning Landing code

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -7,8 +7,8 @@ import BottomOverlay from "./BottomOverlay";
 const Landing = () => {
   const navigate = useNavigate();
   const isLoggedIn = localStorage.getItem("loggedInUser");
-  const [overlayOpen, setOverlayOpen] = useState(false);
   const [loginSelect, setLoginSelect] = useState(false);
+  const [overlayOpen, setOverlayOpen] = useState(false);
 
   const selectLogin = () => {
     setLoginSelect(true);
@@ -19,10 +19,6 @@ const Landing = () => {
     setLoginSelect(false);
     setOverlayOpen(true);
   };
-
-  // const toggleOverlay = () => {
-  //   setOverlayOpen(!overlayOpen);
-  // };
 
   useEffect(() => {
     if (isLoggedIn) navigate("/dashboard");
@@ -54,30 +50,6 @@ const Landing = () => {
         setOverlayOpen={setOverlayOpen}
         content={loginSelect ? <Login /> : <Register />}
       />
-
-      {/* darken overlay */}
-      {/* <div
-        className={`fixed top-0 left-0 bg-black ${
-          overlayOpen
-            ? "opacity-50 right-0 bottom-0 transition-opacity"
-            : "opacity-0"
-        }`}
-        onClick={toggleOverlay}
-      /> */}
-
-      {/* Bottom menu */}
-      {/* <div
-        className={`flex flex-col fixed bottom-0 w-screen rounded-tl-3xl p-6 bg-gray-100 text-gray-800 dark:bg-gray-800 dark:text-gray-100 h-[70%] z-10 shadow-2xl transition-transform transform ${
-          overlayOpen ? "translate-y-0" : "translate-y-full"
-        }`}
-      >
-        <div className="w-full h-fit flex flex-row justify-end items-end gap-6">
-          <button onClick={toggleOverlay} className="px-3 py-1 rounded-full">
-            X
-          </button>
-        </div>
-        <div>{loginSelect ? <Login /> : <Register />}</div>
-      </div> */}
     </>
   );
 };
