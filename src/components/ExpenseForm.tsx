a4209e665a9f2ae5a8a909ca381a68b79a96769e
hugotonette <hugo.tonette@gmail.com>
2024-12-22 23:27:36 -0300

    expense form refactor

--- src/components/ExpenseForm.tsx
+++ src/components/ExpenseForm.tsx
@@ -26,40 +26,52 @@ const ExpenseForm = ({ onAdd }: { onAdd: (expense: any) => void }) => {
   };
 
   return (
-    <form onSubmit={handleSubmit} className="flex flex-col gap-4">
-      <input
-        type="number"
-        placeholder="Amount"
-        value={amount}
-        onChange={(e) => setAmount(e.target.value)}
-        className="border p-2"
-      />
-      <select
-        value={category}
-        onChange={(e) => setCategory(e.target.value)}
-        className="border p-2"
+    <div className="flex flex-col w-full items-center justify-center gap-5">
+      <form
+        onSubmit={handleSubmit}
+        className="flex flex-col gap-4 max-w-xs w-9/12 justify-center items-center"
       >
-        <option key="0" value="" className="">
-          Select a category
-        </option>
-        {categories.map((cat) => (
-          <option key={cat.id} value={cat.name}>
-            {cat.name}
+        <input
+          type="number"
+          placeholder="Amount"
+          value={amount}
+          onChange={(e) => setAmount(e.target.value)}
+          className="w-full p-2 bg-transparent border-2 rounded-lg border-gray-800 dark:border-gray-100"
+        />
+        <select
+          value={category}
+          onChange={(e) => setCategory(e.target.value)}
+          className="w-full p-2 bg-transparent border-2 rounded-lg border-gray-800 dark:border-gray-100"
+        >
+          <option key="0" value="" className="text-black">
+            Select a category
           </option>
-        ))}
-      </select>
-      <select
-        value={type}
-        onChange={(e) => setType(e.target.value)}
-        className="border p-2"
-      >
-        <option value="expense">Expense</option>
-        <option value="income">Income</option>
-      </select>
-      <button type="submit" className="bg-green-500 text-white py-2 px-4">
-        Add
-      </button>
-    </form>
+          {categories.map((cat) => (
+            <option key={cat.id} value={cat.name} className="text-black">
+              {cat.name}
+            </option>
+          ))}
+        </select>
+        <select
+          value={type}
+          onChange={(e) => setType(e.target.value)}
+          className="w-full p-2 bg-transparent border-2 rounded-lg border-gray-800 dark:border-gray-100"
+        >
+          <option value="expense" className="text-black">
+            Expense
+          </option>
+          <option value="income" className="text-black">
+            Income
+          </option>
+        </select>
+        <button
+          type="submit"
+          className="rounded-md bg-green-500 text-white py-2 px-4"
+        >
+          Add
+        </button>
+      </form>
+    </div>
   );
 };
 
