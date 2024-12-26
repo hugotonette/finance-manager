bbdeebc0c392ab5ae58e4b6fe9e756bceefbe9d4
hugotonette <hugo.tonette@gmail.com>
2024-12-25 23:39:44 -0300

    Update Register.tsx

--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -1,6 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
 
 const Register = () => {
   const [formData, setFormData] = useState({
